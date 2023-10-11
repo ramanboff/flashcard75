004a96f73dcca143585e8c6f3e892c775d39253a
ramanboff <bayramofframan@gmail.com>
2023-10-11 22:43:32 +0400

    added turnery into flashcard conponent

--- src/FlashCard.jsx
+++ src/FlashCard.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
 const FlashCard = ({questions}) => {
+
+const [selectedId, setSelectedId] = useState(9103)
+
   return ( 
     <div className="flashcards">
  {questions.map((question) => (
-  <div>
-    <p>{question.question}</p>
+  <div key={question.id}>
+    <p>{question.id === selectedId ? question.answer : question.question}</p>
   </div>
  ))}
     </div>
